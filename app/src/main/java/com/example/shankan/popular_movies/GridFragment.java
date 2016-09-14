5664166608559d8a8ba7334a593199e8e20e7826
Shankar <shankarn@outlook.com>
2016-09-14 11:30:06 +0530

    Intermediate - Created image view

--- app/src/main/java/com/example/shankan/popular_movies/GridFragment.java
+++ app/src/main/java/com/example/shankan/popular_movies/GridFragment.java
@@ -11,7 +11,9 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.GridView;
+import android.widget.Toast;
 
 /**
  * Created by shankan on 9/13/2016.
@@ -26,6 +28,13 @@ public class GridFragment extends Fragment {
         View rootView = inflater.inflate(R.layout.grid_fragment, container, false);
         GridView gridView = (GridView) rootView.findViewById(R.id.grid_layout);
         gridView.setAdapter(new GridElement(getContext()));
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                String newS = "sh" + i ;
+                Toast.makeText(getContext(),newS, Toast.LENGTH_SHORT).show();
+            }
+        });
         return rootView;
     }
 }
---
+++ app/src/main/java/com/example/shankan/popular_movies/MoviesDetail.java
@@ -0,0 +1,7 @@
+package com.example.shankan.popular_movies;
+
+/**
+ * Created by shankan on 9/14/2016.
+ */
+public class MoviesDetail {
+}
